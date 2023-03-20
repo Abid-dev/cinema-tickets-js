2b136db7439f4383878293218680162eff42d99a
Abid <abdurRahman.abid@outlook.com>
2023-03-20 21:49:10 +0000

    make base index files

---
+++ index.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+   let welcomeMessage = document.getElementById('welcome-section');
+   let inputSection = document.getElementById('input-section');
+    welcomeMessage.style.display = 'none';
+    inputSection.style.display = 'flex';
+}, 2000);
