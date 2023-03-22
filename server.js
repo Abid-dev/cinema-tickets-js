a01373b6b60cbd643fe1d4d2065e90c322d2758a
Abid <abdurRahman.abid@outlook.com>
2023-03-22 12:16:17 +0000

    render files with node with the static files too

--- server.js
+++ server.js
@@ -1,67 +1,26 @@
-// server.js
-import http from 'http';
-import url from 'url';
-import querystring from 'querystring';
+import express from 'express';
 import path from 'path';
-import fs from 'fs';
+import mime from 'mime';
 
+const app = express();
 global.__dirname = path.resolve();
-const MIME_TYPES = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.gif': 'image/gif'
-  };
-const server = http.createServer((req, res) => {
 
-    const filePath = req.url === '/' ? './index.html' : `.${req.url}`;
-  const extname = path.extname(filePath);
-  const contentType = MIME_TYPES[extname] || 'application/octet-stream';
+/** Serve static files from the public dir */
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-  fs.readFile(filePath, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404 Not Found');
-    } else {
-      res.writeHead(200, {'Content-Type': contentType});
-      res.end(data);
-    }
-  });
+app.get('/', (req, res) => {
+    const htmlPath = path.join(new URL('./index.html', import.meta.url).pathname);
+    res.sendFile(htmlPath);});
 
+app.post('/submit', (req, res) => {
+  const adultTickets = req.body.adultTickets;
+  console.log(adultTickets);
+  const message = `Hello, ${adultTickets}!`;
 
-//   if (req.method === 'POST' && req.url === '/submit') {
-//     let body = '';
-
-//     req.on('data', (chunk) => {
-//       body += chunk.toString();
-//     });
-
-//     req.on('end', () => {
-//       const formData = querystring.parse(body);
-//       const adultTickets = formData.adultTickets;
-
-//       // Do something with the form data
-//       console.log(`adultTickets: ${adultTickets}`);
-
-//       // Send a response back to the client
-//       res.writeHead(200, { 'Content-Type': 'text/plain' });
-//       res.end('Form submitted successfully');
-//     });
-//   } else {
-//     res.writeHead(404, { 'Content-Type': 'text/plain' });
-//     res.end('Page not found');
-//   }
+  res.send(message);
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
-
-process.on('SIGINT', () => {
-    server.close(() => {
-      console.log('Server closed');
-      process.exit(0);
-    });
-  });
