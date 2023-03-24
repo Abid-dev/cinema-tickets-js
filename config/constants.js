0a12d9535dc6e76da6dbbe09940f391a0fe5e0fc
Abid <abdurRahman.abid@outlook.com>
2023-03-24 06:33:07 +0000

    Make a constants file to hold the prices of the tickets #2

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+export const TICKET_TYPE_CHILD = 'CHILD';
+export const TICKET_TYPE_INFANT = 'INFANT';
+export const TICKET_TYPE_ADULT = 'ADULT';
+
+export const TICKET_PRICE_ADULT = 20;
+export const TICKET_PRICE_CHILD = 10;
+export const TICKET_PRICE_INFANT = 0;
